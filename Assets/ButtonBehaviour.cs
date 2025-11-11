fa385d39c30aa058c82e94c92747ed7760216c31
sin-en <s10268353@connect.np.edu.sg>
2025-11-11 22:42:11 +0800

    Add Image Trackers and Simple Interaction

---
+++ Assets/ButtonBehaviour.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject targetObject; // Assign the root of the jar in Inspector
+
+    public void ToggleMeshRenderer()
+    {
+        MeshRenderer[] renderers = targetObject.GetComponentsInChildren<MeshRenderer>();
+        foreach (MeshRenderer r in renderers)
+        {
+            r.enabled = !r.enabled;
+        }
+    }
+}
+
+
